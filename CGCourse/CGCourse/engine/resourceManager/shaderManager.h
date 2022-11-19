35f3e0e92357da64f3c1cd4ded4f7e751c7009ff
Daniil Glushchenko <daniil.glushchenko@mytona.com>
2022-11-19 13:49:41 +0600

    Add binary shader support

--- CGCourse/CGCourse/engine/resourceManager/shaderManager.h
+++ CGCourse/CGCourse/engine/resourceManager/shaderManager.h
@@ -8,13 +8,13 @@ namespace KUMA {
 	namespace RESOURCES {
 		class ShaderLoader : public ResourceManager<Shader> {
 		public:
-			static ResourcePtr<Shader> CreateFromFile(const std::string& path);
+			static ResourcePtr<Shader> CreateFromFile(const std::string& path, bool useBinary=true);
 			static ResourcePtr<Shader> CreateFromSource(const std::string& vertexShader, const std::string& fragmentShader,
 				const std::string& geometryShader = "", const std::string& tessCompShader="", const std::string& tessEvoluationShader="");
 			static void Recompile(Shader& shader, const std::string& filePath);
 
 		protected:
-			static ResourcePtr<Shader> Create(const std::string& filePath);
+			static ResourcePtr<Shader> Create(const std::string& filePath, bool useBinary);
 			static std::array<std::string, 5> ParseShader(const std::string& filePath);
 			static uint32_t CreateProgram(const std::string& vertexShader, const std::string& fragmentShader, 
 				const std::string& geometryShader, const std::string& tessCompShader, const std::string& tessEvoluationShader);
@@ -24,6 +24,7 @@ namespace KUMA {
 			
 			//for error print
 			static std::string FILE_PATH;
+			static bool checkBinarySupport();
 		};
 
 	}
